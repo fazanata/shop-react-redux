d49219461d42c27d052f885a25c9efcb89b6558b
fazanata <faz0@ya.ru>
2021-03-30 14:40:21 +0300

    add shop

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import './main.css'
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {applyMiddleware, createStore} from 'redux'
+import {createStore, applyMiddleware} from 'redux'
 import {createBrowserHistory} from 'history'
 import thunk from 'redux-thunk'
-import {ConnectedRouter, routerMiddleware} from 'connected-react-router'
+import {routerMiddleware, ConnectedRouter} from 'connected-react-router'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import {Provider} from 'react-redux'
 
